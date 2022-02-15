72c1e8e806c9ab98df3c9791415e40fbb165a347
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-15 11:34:05 -0600

    some notes

--- src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
@@ -7,6 +7,11 @@ namespace WebApp.Pages.SamplePages
     {
         public void OnGet()
         {
+            //Executes in response to a Get Request from the browser
+            //When the page is "First" accessed, the browser issues the get request
+            //When the page is refreshed, Without a Post request the browser issues a get request
+            //when the page is retrived in response to a form's Posting using RedirectToPage()
+            //If Not RedirectToPage() is used on the POST, there is no Get Request
         }
     }
 }
