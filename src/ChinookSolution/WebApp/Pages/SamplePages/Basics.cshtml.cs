0e194da4f6c60d5217bc28794c4ed8fa4eea39c1
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-15 11:50:12 -0600

    some changes in the basic page

--- src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
@@ -5,6 +5,17 @@ namespace WebApp.Pages.SamplePages
 {
     public class BasicsModel : PageModel
     {
+        public string MyName;
+
+        //basically this is an object, treat it as such
+
+        //data fields
+
+        //properties
+
+        //constructors
+
+        //behaviors(AKA methods)
         public void OnGet()
         {
             //Executes in response to a Get Request from the browser
@@ -12,6 +23,21 @@ namespace WebApp.Pages.SamplePages
             //When the page is refreshed, Without a Post request the browser issues a get request
             //when the page is retrived in response to a form's Posting using RedirectToPage()
             //If Not RedirectToPage() is used on the POST, there is no Get Request
+
+            //server-side processing
+            //contains no html
+
+
+            Random rnd=new Random();
+            int oddeven=rnd.Next(0,25);
+            if(oddeven% 2==0){
+                    MyName=$"Don is even {oddeven}";
+            }
+            else
+            {
+                MyName= null;
+            }
+
         }
     }
 }
