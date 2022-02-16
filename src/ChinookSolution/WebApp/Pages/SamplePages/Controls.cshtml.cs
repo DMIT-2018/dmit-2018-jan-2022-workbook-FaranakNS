fcad9c2e17e9902ff65e7f5a74b46a974ecef25b
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-16 11:55:40 -0600

    some changes with adding controls

---
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.SamplePages
+{
+    public class ControlsModel : PageModel
+    {
+        [TempData]
+        public string Feedback { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
