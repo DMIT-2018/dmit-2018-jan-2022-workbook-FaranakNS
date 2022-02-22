090f323a6338c7b07b5bb37fc2a1cba688fdab9d
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-22 16:00:03 -0600

    stuff adding in the above page to make things work

---
+++ src/ChinookSolution/ChinookSystem/ChinookExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+#endregion
+
+namespace ChinookSystem
+{
+    public static class ChinookExtensions
+    {
+
+        public static void ChinookSystemBackendDependencies(this IServiceCollection services, 
+            
+            Action<DbContextOptionsBuilder> options)
+        {
+
+
+        }
+    }
+}
