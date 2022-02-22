247ebbbc2bb75c650834126a1b3a26ec1fba08e5
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-21 20:44:06 -0600

    cmt all

---
+++ src/ChinookSolution/WpfLibrary1/ChinookExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfLibrary1
+{
+    public class ChinookExtentions
+    {
+        public static void ChinookSystemBackendDependencies(this IServiceCollection services,
+            Action<DbContectOptionsBuilder> options)
+        {
+            services.AddDbContext<ChinookContext>(options);
+
+        }
+    }
+}
