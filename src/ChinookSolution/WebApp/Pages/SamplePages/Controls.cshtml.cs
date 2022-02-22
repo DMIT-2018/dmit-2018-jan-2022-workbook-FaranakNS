47eda908807325e458eb6920e9fa078f38e732fa
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-21 22:21:06 -0600

    some changes

--- src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -33,8 +33,47 @@ namespace WebApp.Pages.SamplePages
 
         [BindProperty]
         public string MessageBody { get; set; }
+
+        [BindProperty]
+        public int MyRide { get; set; }
+        //pretend that following collection is data from database
+        //the collection is baed on 2 property based on 2 property class called SelectionList
+        //the data for the list will created in a seprate method
+
+        public List<SelectionList> Rides { get; set; }
+
+        public string VacationSpot { get; set; }
+        public List<string> VacationSpot { get; set; }
+        public int ReviewRating { get; set; }
+
+
+
         public void OnGet()
         {
+            PopulateList();
+        }
+
+
+        public void PopulateList()
+        {
+            //create a pretend collection from a database represents different types
+            //of a transportation (rides)
+
+            Rides = new List<SelectionList>();
+            Rides.Add(new SelectionList() { ValueId = 1, DisplayText = "Car" });
+            Rides.Add(new SelectionList() { ValueId = 2, DisplayText = "Bus" });
+            Rides.Add(new SelectionList() { ValueId = 3, DisplayText = "Bike" });
+            Rides.Add(new SelectionList() { ValueId = 4, DisplayText = "Motorcycle" });
+            Rides.Add(new SelectionList() { ValueId = 5, DisplayText = "Board" });
+            Rides.Sort((x, y) => x.DisplayText.CompareTo(y.DisplayText));
+
+            VacationSpot = new List<string>();
+            VacationSpot.add("California");
+            VacationSpot.add("Caribean");
+            VacationSpot.add("Crusing");
+            VacationSpot.add("Europe");
+            VacationSpot.add("Florida");
+            VacationSpot.add("Mexico");
 
         }
 
@@ -51,5 +90,22 @@ namespace WebApp.Pages.SamplePages
             Feedback = $"Meal{ Meal}; Acceptance {AcceptanceBox };  Message {MessageBody};";
             return Pages();
         }
+
+
+
+        public IActionResult OnPostListSlider()
+        {
+
+            Feedback = $"Ride{ MyRide}; Vacation {VacationSpot };  Reveiw Rating {ReviewRating};";
+            PopulateList();
+            return Pages();
+        }
     }
+}
+
+
+public class SelectionList
+{
+    public int ValueId { get; set; }
+    public string DisplayText { get; set; }
 }
