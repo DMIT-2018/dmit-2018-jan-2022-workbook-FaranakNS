54eef8994b5abbfd99df176528aabb3f292cbde4
Faranak Nasoori <faranaknasoori@gmail.com>
2022-02-23 19:13:56 -0600

    cmt

---
+++ src/ChinookSolution/ChinookSystem/BLL/AboutServices.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChinookSystem.Entities;
+using ChinookSystem.ViewModels;
+
+#nullable disable
+#region Additional Namespaces
+
+using ChinookSystem.DAL;
+#endregion
+
+
+namespace ChinookSystem.BLL
+{
+    public class AboutServices
+    {
+        //this class needs to be accessed by an "outside user" (WebApp)
+        //therefore the class needs to be public
+
+        #region Constructor and Context Dependency
+        private readonly ChinookContext _context;
+
+        //obtain the context link from IServiceCollection when this
+        //  set of service is injected into the "outside user"
+        internal AboutServices(ChinookContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+        #region Services
+        #endregion
+
+        #region Services
+        //services are methods
+
+        //query to obtain the DbVersion data
+        public Entities.DbVersionInfo GetDbVersion()
+        {
+            //DbVersionInfo is a public "view" of data defined in a class
+            //DBVersionInfo can be a class used BOTH internally and by external users
+            //DbVersion is an internal entity description used ONLY in the libaray
+            Entities.DbVersionInfo info = _context.DbVersions
+                                .Select(x => new Entities.DbVersionInfo
+                                {
+                                    Major = x.Major,
+                                    Minor = x.Minor,
+                                    Build = x.Build,
+                                    ReleaseDate = x.ReleaseDate
+                                })
+                                .SingleOrDefault();
+            return info;
+
+        }
+        #endregion
+    }
+}
