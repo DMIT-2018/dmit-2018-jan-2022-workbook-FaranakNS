4f0d1c2c1c46904838c41c6956405fb712742404
Faranak Nasoori <faranaknasoori@gmail.com>
2022-03-02 07:22:57 -0600

    cmt

--- src/ChinookSolution/WebApp/Pages/Index.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+#nullable disable
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+
+#endregion
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        #region Private Variable & DI Constructor
+       
+        private readonly ILogger<IndexModel> _logger;
+        private readonly AboutServices _aboutServices;
+
+        public IndexModel(ILogger<IndexModel> logger,
+            
+            AboutServices aboutServices)
         {
             _logger = logger;
+            _aboutServices = aboutServices;
         }
+        #endregion
 
+        #region FeedBack and ErrorHandling
+     
+
+        public string FeedBack { get; set; }
+        public bool HasFeedBack => !string.IsNullOrWhiteSpace(FeedBack);
+        #endregion
         public void OnGet()
         {
+            //consume a service
+            DbVersionInfo info = _aboutServices.GetDbVersion();
 
+            if (info == null)
+            {
+                FeedBack = "Version unknown";
+            }
+            else
+            {
+                FeedBack= $"Version: { info.Major}. { info.Minor}.{ info.Build}" + $"Release date of {info.ReleaseDate.ToShortDateString() } ";
+               
+            }
         }
     }
 }
